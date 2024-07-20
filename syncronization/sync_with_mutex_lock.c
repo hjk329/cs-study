45936cf1baeb317b49775558a8cfb81e99da4e68
hyungji.k <icthelight@naver.com>
2024-07-20 16:13:03 +0900

    chore: 주석 추가

--- syncronization/sync_with_mutex_lock.c
+++ syncronization/sync_with_mutex_lock.c
@@ -8,10 +8,18 @@ int shared = 0;
 
 void *foo()
 {
-    pthread_mutex_lock(&mutex); // 뮤텍스 락을 걸어서 한 번에 하나의 스레드에서만 임계 구역에 접근하도록 동기화
+    // 뮤텍스 락을 걸어서 한 번에 하나의 스레드에서만 임계 구역에 접근하도록 동기화
+    // 각 스레드는 임계구역을 진입하기 위해 mutex 변수를 확보해야함
+    // 확보하지 못했다면 임계구역 앞에서 대기
+
+    pthread_mutex_lock(&mutex); 
+
     for (int i = 0; i < 10000; ++i) {
         shared += 1;
     }
+
+    // 잠금을 해제하여 다른 스레드가 shared 변수를 수정할 수 있도록 함
+    // -> 레이스 컨디션을 해결하고 동기화
     pthread_mutex_unlock(&mutex);
     return NULL;
 }
